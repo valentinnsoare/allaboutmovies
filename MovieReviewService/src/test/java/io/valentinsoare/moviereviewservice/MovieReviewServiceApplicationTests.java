ac5b45ecab6508a801e947f2687d169c40b0f6e1
valentinsoare <soarevalentinn@gmail.com>
2025-01-13 03:14:19 +0200

    MoviewReviewService module created.

---
+++ MovieReviewService/src/test/java/io/valentinsoare/moviereviewservice/MovieReviewServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package io.valentinsoare.moviereviewservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieReviewServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
