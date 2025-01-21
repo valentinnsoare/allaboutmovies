bbb34196376adb83f2d3a07892201db96ab6f079
valentinsoare <soarevalentinn@gmail.com>
2025-01-21 13:22:31 +0200

    getAllReviewsFromMovieInfoId(String movieInfoId) written in MovieReviewRestClient service

--- MovieService/src/main/java/io/valentinsoare/movieservice/client/MovieReviewRestClient.java
+++ MovieService/src/main/java/io/valentinsoare/movieservice/client/MovieReviewRestClient.java
@@ -1,12 +1,17 @@
 package io.valentinsoare.movieservice.client;
 
 import io.valentinsoare.movieservice.domain.MovieReview;
+import io.valentinsoare.movieservice.exception.MovieReviewClientException;
+import io.valentinsoare.movieservice.exception.MovieReviewServerException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.stereotype.Component;
 import org.springframework.web.reactive.function.client.WebClient;
 import org.springframework.web.util.UriComponentsBuilder;
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
 @Component
 public class MovieReviewRestClient {
@@ -30,6 +35,21 @@ public class MovieReviewRestClient {
         return webClient.get()
                 .uri(url, movieInfoId)
                 .retrieve()
+                .onStatus(HttpStatusCode::is4xxClientError, clientResponse -> {
+                    if (clientResponse.statusCode().equals(HttpStatus.NOT_FOUND)) {
+                        return Mono.empty();
+                    }
+
+                    return clientResponse.bodyToMono(String.class)
+                            .flatMap(errorBody -> Mono.error(new MovieReviewClientException(
+                                    errorBody, clientResponse.statusCode().value())));
+                })
+                .onStatus(HttpStatusCode::is5xxServerError, clientResponse -> clientResponse.bodyToMono(String.class)
+                        .flatMap(errorBody -> Mono.error(new MovieReviewServerException(
+                                String.format("Server exception in MovieReviewService: %s",
+                                        errorBody)
+                        )))
+                )
                 .bodyToFlux(MovieReview.class);
     }
 }
